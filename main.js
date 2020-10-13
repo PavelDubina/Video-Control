e704b225067a3963e811c0f50c7b74f52a70cdba
Pavel Dubina <Pavel_Dubina@epam.com>
2020-10-13 15:18:13 +0300

    init: finish

---
+++ main.js
@@ -0,0 +1,17 @@
+const video = document.querySelector('.flex'),
+    speed = document.querySelector('.speed'),
+    bar = document.querySelector('.speed-bar');
+
+    speed.addEventListener('mousemove', function(e){
+        const y = e.pageY - this.offsetTop,
+        persent = y / this.offsetHeight,
+        min = 0.4,
+        max = 4;
+        height = Math.round(persent * 100) + '%', 
+        playbackRate = persent * (max - min) + min;
+
+        bar.style.height = height;
+        bar.textContent = playbackRate.toFixed(2) + 'x';
+        video.playbackRate = playbackRate;
+
+    })
